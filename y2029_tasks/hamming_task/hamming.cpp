f4ee24b574aa199affe92513c639eb77321995bf
tanechka5226 <piano.tolstova@gmail.com>
2025-12-13 02:39:25 +0300

    Create hamming.cpp

---
+++ y2029_tasks/hamming_task/hamming.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <string>
+
+bool isPowerOfTwo(int num) {
+    while (num > 1) {
+        if (num % 2 != 0) {
+            return false;
+        }
+        num = num / 2;
+    }
+    return true;
+}
+
+void encode() {
+    int m, n;
+    std::string bits;
+    std::cin >> m >> n >> bits;
+    
+    if (bits.length() != m) {
+        return;
+    }
+    
+    std::string result(n, '0');
+    
+    int bit_index = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!isPowerOfTwo(i)) {
+            if (bit_index < m) {
+                result[i-1] = bits[bit_index];
+                bit_index++;
+            }
+        }
+    }
+    
+    for (int p = 1; p <= n; p *= 2) {
+        int count_ones = 0;
+        
+        for (int i = p; i <= n; i++) {
+            if ((i & p) != 0 && result[i-1] == '1') {
+                count_ones++;
+            }
+        }
+        
+        if (count_ones % 2 == 1) {
+            result[p-1] = '1';
+        } else {
+            result[p-1] = '0';
+        }
+    }
+    
+    std::cout << result << std::endl;
+}
+
+void decode() {
+    int n;
+    std::string bits;
+    std::cin >> n >> bits;
+    
+    if (bits.length() != n) {
+        return;
+    }
+    
+    int error_position = 0;
+    
+    for (int p = 1; p <= n; p *= 2) {
+        int count_ones = 0;
+        
+        for (int i = p; i <= n; i++) {
+            if ((i & p) != 0 && bits[i-1] == '1') {
+                count_ones++;
+            }
+        }
+        
+        if (count_ones % 2 == 1) {
+            error_position += p;
+        }
+    }
+    
+    std::string corrected = bits;
+    if (error_position > 0 && error_position <= n) {
+        if (corrected[error_position-1] == '0') {
+            corrected[error_position-1] = '1';
+        } else {
+            corrected[error_position-1] = '0';
+        }
+    }
+    
+    std::string data_bits = "";
+    for (int i = 1; i <= n; i++) {
+        if (!isPowerOfTwo(i)) {
+            data_bits += corrected[i-1];
+        }
+    }
+    
+    int check_bits = 0;
+    for (int p = 1; p <= n; p *= 2) {
+        check_bits++;
+    }
+    
+    std::cout << check_bits << std::endl;
+    std::cout << data_bits << std::endl;
+}
+
+void is_valid() {
+    int n;
+    std::string bits;
+    std::cin >> n >> bits;
+    
+    if (bits.length() != n) {
+        return;
+    }
+    
+    bool has_error = false;
+    
+    for (int p = 1; p <= n; p *= 2) {
+        int count_ones = 0;
+        
+        for (int i = p; i <= n; i++) {
+            if ((i & p) != 0 && bits[i-1] == '1') {
+                count_ones++;
+            }
+        }
+        
+        if (count_ones % 2 == 1) {
+            has_error = true;
+            break;
+        }
+    }
+    
+    if (has_error) {
+        std::cout << "0" << std::endl;
+    } else {
+        std::cout << "1" << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        return 1;
+    }
+    
+    std::string command = argv[1];
+    
+    if (command == "encode") {
+        encode();
+    } else if (command == "decode") {
+        decode();
+    } else if (command == "is_valid") {
+        is_valid();
+    }
+    
+    return 0;
+}
